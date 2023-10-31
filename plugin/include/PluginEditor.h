0e3b8588e954d09e1bc6d1573ebdcc3b0df12e33
whoadrian <whoa.adrian@gmail.com>
2023-10-31 16:32:49 +0100

    init plugin

---
+++ plugin/include/PluginEditor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "PluginProcessor.h"
+
+//==============================================================================
+class WhoaAudioPluginEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit WhoaAudioPluginEditor (WhoaAudioPluginProcessor&);
+    ~WhoaAudioPluginEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+	WhoaAudioPluginProcessor& processorRef;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WhoaAudioPluginEditor)
+};
