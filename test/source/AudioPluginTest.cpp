0e806ad4f883004652f996d5ae21cc35134216d4
whoadrian <whoa.adrian@gmail.com>
2023-10-31 17:19:58 +0100

    google test

---
+++ test/source/AudioPluginTest.cpp
@@ -0,0 +1,9 @@
+#include <PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace audio_plugin_test {
+	TEST(AudioProcessor, Tests) {
+		// Instantiate processor
+		WhoaAudioPluginProcessor processor{};
+	}
+} // namespace audio_plugin_test
