2bddbcfae096299fc1582cc1d79835e14d3aa172
whoadrian <whoa.adrian@gmail.com>
2023-10-31 17:52:47 +0100

    cmake tool init

---
+++ tools/cmake_setup_tool.py
@@ -0,0 +1,5 @@
+# generates cmake include folders, files, assets for juce
+# uses tags in the form of <tag>..</tag> to detect where to make changes
+if __name__ == '__main__':
+    print("hello")
+    pass
