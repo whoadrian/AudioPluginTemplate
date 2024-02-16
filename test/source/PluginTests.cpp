baa6ad4eade694f5246ad3738c600422978bc0e2
whoadrian <whoa.adrian@gmail.com>
2024-02-16 19:32:42 +0100

    removed google test

--- test/source/PluginTests.cpp
+++
@@ -1,9 +0,0 @@
-#include <PluginProcessor.h>
-#include <gtest/gtest.h>
-
-namespace PluginTests {
-	TEST(AudioProcessor, Tests) {
-		// Instantiate processor
-		WhoaAudioPluginProcessor processor{};
-	}
-} // namespace audio_plugin_test
